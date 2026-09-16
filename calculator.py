d930453030ee2cc8624cfc5e7b2ff3dc5a4a505e
syedhamza-125 <hamzahussain6251@gmail.com>
2026-09-16 10:42:00 -0700

    Implement basic calculator functions
    Added basic arithmetic operations for two numbers.

---
+++ calculator.py
@@ -0,0 +1,7 @@
+num1 = float(input("First number: "))
+num2 = float(input("Second number: "))
+
+print("Sum:", num1 + num2)
+print("Difference:", num1 - num2)
+print("Product:", num1 * num2)
+print("Division:", num1 / num2)
